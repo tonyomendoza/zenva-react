efb8f190e8eb62422a4ecd076d7aeaa06f4b07ee
tonyomendoza <mendozatony@outlook.com>
2021-09-16 12:49:45 -0700

    Lesson 5 Diving into JSX

--- src/index.js
+++ src/index.js
@@ -2,18 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function HelloWorld(){
-  return(
-    <div>Hello World!</div>
-  );
+  return <div>
+    <Hello_alt />
+    </div>;
 }
 
+// function Hello(){
+//   return React.createElement(
+//     'div',
+//     {},
+//     // "Hello World!"
+//   );
+// }
+
 function Hello(){
-  return React.createElement(
-    'div',
-    {},
-    // "Hello World!"
-  );
+  // const name = 'Tony!';
+  // return <span>Hello {name}</span>;
+  var isHello = false;
+  return (
+    <span>{isHello? 'Hello': 'Goodbye'}</span>
+  )
 }
+
+function Hello_alt(){
+  var isHello = true;
+  return (
+    <span>
+      {isHello && 'Hello'}
+      {!isHello && 'Goodbye'}
+    </span>
+  )
+}
+
+// function World(){
+//   return <span>World</span>;
+// }
 
 ReactDOM.render(
   <HelloWorld />, document.querySelector('#root')
